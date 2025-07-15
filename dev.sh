820ba9798d2491e3e40043da2b5bca344bbbefff
Max Loiacono <max.loiacono@protonmail.com>
2025-07-15 18:45:42 -0400

    prep for django

--- dev.sh
+++ dev.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Assuming you have docker compose aliased... Not dealing with docker compose vs docker-compose
-
-docker compose --file docker-compose.dev.yml up
-docker compose --file docker-compose.dev.yml down -v
-docker compose --file docker-compose.dev.yml down -v
+python manage.py runserver
--- photoserv/settings.py
+++ photoserv/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0_8hso@x)hs&sta0s_4t+x_td(r$)yg-3-kcsr_zd2%nqn@il9'
+SECRET_KEY = os.environ.get("MASTER_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get("DEBUG_MODE").strip() == "true")
 
 ALLOWED_HOSTS = []
 
